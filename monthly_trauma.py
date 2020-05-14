890d2a807430ab9cd7f32721cc6bbcbd7ab47cf0
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2020-05-14 12:04:15 +1000

    Update monthly_trauma.py

--- monthly_trauma.py
+++ monthly_trauma.py
@@ -18,10 +18,11 @@ df_stack = df[['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
 
 # df_time = pd.date_range('1901-01-01', periods=1380, freq='M')
 
+# Rearrange so all var values are in one column
 mgpp = df_stack.to_numpy()
-print(len(mgpp))
 lon = df['Lon'].to_numpy()
 lat = df['Lat'].to_numpy()
+
 # time = df_time.to_numpy()
 # time = np.arange(1901,2016)
 # time = np.repeat(time, repeats = 12)
@@ -29,6 +30,7 @@ print(len(np.repeat(lon, repeats = 12)))
 print(len(np.arange(0, 81366180)))
 # print(len(np.tile(time, 58961)))
 
+# Write into new dataframe
 df_sorted = pd.DataFrame()
 df_sorted['Lon'] = np.repeat(lon, repeats = 12)
 df_sorted['Lat'] = np.repeat(lat, repeats = 12)
