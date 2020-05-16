048071e0b2120688c21d17bbe970f9c1425ede86
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2020-05-16 10:58:10 +1000

    Create convert_grid.py

---
+++ convert_grid.py
@@ -0,0 +1,16 @@
+we=open('gridlist_global_cf.txt','w')
+with open('gridlist_global_test.txt') as f:
+    for line in f:
+#read a line
+         line = line.replace("\n", "")
+         a,b=line.split('\t')
+
+#get two values as string
+         c=360+(2*(float(a)-0.25))
+         d=180+(2*(float(b)-0.25))
+         print(a)
+
+#calculate the other two values
+         e = ''.join(("(",str(a),",",str(b),")"))
+         print(e)
+         we.write(str(int(c))+' '+str(int(d))+' '+e+"\n")
