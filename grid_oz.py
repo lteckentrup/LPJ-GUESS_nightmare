0eadc312c2e69eb5f922ef9ae576d004c591ba89
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2021-07-23 10:04:20 +1000

    Create grid_oz.py

---
+++ grid_oz.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+we=open('gridlist_global_cf_ozcut.txt','w')
+lon = np.arange(112.25,154.25,0.5)
+lat = np.arange(10.25,44.25,0.5)*(-1)
+
+with open('gridlist_global_test.txt') as f:
+    for line in f:
+
+         line = line.replace("\n", "")
+         a,b=line.split('\t')
+
+         if float(a) in lon and float(b) in lat:
+
+             c=(float(a)-112.25)/0.5
+             d=(float(b)-(-43.75))/0.5
+
+             e = ''.join(("(",str(a),",",str(b),")"))
+             we.write(str(int(c))+' '+str(int(d))+' '+e+"\n")
+         else:
+             pass
