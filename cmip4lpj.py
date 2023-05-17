240c39b46aa08d5a9542ec900ed086246878847f
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2023-05-17 19:30:25 +1000

    Create cmip4lpj.py

---
+++ cmip4lpj.py
@@ -0,0 +1,39 @@
+import xarray as xr
+import xesmf 
+
+def cmip4lpj(var_cmip,var_lpj,sn_lpj):
+    ### insert filename of raw data
+    fname_IN = ''
+    ds = xr.open_dataset(fname_IN)
+    ds = ds.rename({var_cmip:var_lpj})
+    ds[var_lpj].attrs['standard_name'] = sn_lpj
+    
+    ### Only for daily and if you want to harmonise all calendars
+    ds = ds.convert_calendar('noleap')
+
+    ### Adjust unit for some variables
+    if var_lpj == 'rhum':
+        ds[var_lpj] = ds[var_lpj]/100
+        ds[var_lpj].attrs['units'] = '1'
+
+    ### Remove bounds (LPJ doesn't need them)
+    ds = ds.drop_vars('time_bnds')
+    ds = ds.drop_vars('lat_bnds')
+    ds = ds.drop_vars('lon_bnds')
+    ds = ds.transpose('lat','lon','time')
+
+    ### insert filename of LPJ data
+    fname_OUT = ''
+    ds.to_netcdf(fname_OUT,
+                 encoding={'time':{'dtype': 'double'},
+                           'lat':{'dtype': 'double'},
+                           'lon':{'dtype': 'double'},
+                           var_lpj:{'dtype': 'float32'}})
+
+cmip4lpj('tas','temp','air_temperature')
+cmip4lpj('pr','prec','precipitation_flux')
+cmip4lpj('rsds','insol','surface_downwelling_shortwave_flux')
+cmip4lpj('hurs','rhum','relative_humidity')
+cmip4lpj('tasmax','tmax','air_temperature')
+cmip4lpj('tasmin','tmin','air_temperature')
+cmip4lpj('sfcWind','wind','wind_speed')
